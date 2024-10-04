3e114bbcf5cedab708a9e6ad7bdb8b067795548e
DevangRnd <rnd.delhi@ielecssol.com>
2024-10-04 18:18:20 +0530

    made changes to the store

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@
 import React, { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import Cookies from "js-cookie";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
